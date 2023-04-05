65c3b8899e025c6304643ff69ab57a9c8ee048ce
adwalker395 <adwalker395@gmail.com>
2023-04-05 23:00:02 +0530

    node js project

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+export const About = () => {
+
+
+  return (
+    <>
+      <div>
+        <h2>About</h2>
+       <p> A sleek and intuitive user interface is the first thing to love about myNotebook.</p>
+
+        <p>Besides its clean interface, myNotebook provides a host of cool note-making features. You can easily edit, delete, and save your notes on this platform. The website maintains a cloud-based backup that ensures your notes are never lost even in the event of computer crashes or system malfunctions.</p>
+      </div>
+    </>
+  )
+}
